b59d9cd73e583717bee2e92294782027453b48d7
zureidy <zureidy2004@gmail.com>
2025-10-06 13:14:06 -0600

    configuracion de mensaje y sonido

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://20.9.134.234');
+var socket = io.connect('https://wonderful-grass-03f08481e.2.azurestaticapps.net');
 var audioNotificacion;
 var persona = document.getElementById('persona'),
   appChat = document.getElementById('app-chat'),
